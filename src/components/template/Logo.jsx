023bd26205121441c7e94804c9028719a06afee1
Lucas-ASouza <lucasalves-x@hotmail.com>
2022-12-02 12:37:13 -0300

    basic layout

---
+++ src/components/template/Logo.jsx
@@ -0,0 +1,11 @@
+import './Logo.css'
+import React from 'react'
+
+import logo from '../../assets/images/logo.jpg'
+
+export default props =>
+    <aside className="logo">
+        
+        <a href="" className="href"></a>
+            <img src={logo} alt="logo" />
+    </aside>
---
+++ src/components/template/Main.jsx
@@ -0,0 +1,12 @@
+import './Main.css'
+import Header from './Header'
+import React from 'react'
+
+
+export default props =>
+    <React.Fragment>
+        <Header {...props}/>
+        <main className="content">
+            Conteúdo
+        </main>
+    </React.Fragment>
