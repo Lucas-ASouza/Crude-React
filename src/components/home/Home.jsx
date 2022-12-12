c59d51bed41adb825c543ad61b1ee9c44195dfeb
Lucas-ASouza <lucasalves-x@hotmail.com>
2022-12-12 08:15:13 -0300

    Feat: Home Component

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default props =>
+
+<Main icon="home" title="Home" 
+subtitle="Training project using React">
+    <div className='display-4'>Welcome!</div>
+    <hr />
+    <p className="mb-0">Developed to train an working sign up system in React</p>
+ </Main>
